ebce0bc4bba360dba9cc65c737c22c4923434f93
daeppang119 <dbsk9436@naver.com>
2023-10-20 08:55:55 +0900

    saerch기능

--- api.js
+++ api.js
@@ -14,17 +14,30 @@ const form = document.getElementById('form');
 const input = document.getElementById('searchInput');
 const searchBtn = document.getElementById('searchBtn');
 
-function getTopRatedMovies() {
+function getTopRatedMovies(value) {
+  console.log('value is : ',value);
   fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
     .then(response => response.json())
-    .then(renderMovies) // .then(data => renderMovies(data))
+    .then((data) => {
+      if(value) {
+        const newData = {...data,results: data.results.filter(v => {
+          return v.title.toLowerCase().indexOf(value.toLowerCase()) !== -1;
+        })};
+        console.log(newData.results);
+        //data.results = data.results.filter(v => v.title === value);
+          renderMovies(newData);
+        return ;
+      }
+      renderMovies(data);
+    }) // .then(data => renderMovies(data)) // 유난미 자꿈 움직이시ㅕㅁㄴ 저도 움직여요 ㅜㅜ
     .catch(err => console.error(err));
 }
 
 function renderMovies(data) {
-  // console.log(data)
+  // console.log(data)asdfasdfasdfasdfasdfasdfasd
   // console.log(data.results);
   // console.log(JSON.stringify(data));
+  cardList.innerHTML = '';
   data.results.forEach(element => {
     // console.log(element.title);
 
@@ -52,13 +65,18 @@ getTopRatedMovies();
 
 input.addEventListener('keyup', (e)=>{
   e.preventDefault(); // 새로고침 방지
-
   if(e.key === 'Enter') {
     if(input.value !== '') {
-      getTopRatedMovies();
+      // 유나님 이거 이렇게 하면 지금 똑같은 내용을 똑같이 랜더링을 하는 거잖아요
+      // 뭔가 value에 따른 조작을 해야하기 때문에
+      // 그부분을 생각하셔야 할 것 같아요
+
+      // 그런데 현재 코드에서 저라면 
+      getTopRatedMovies(input.value);
     } else {
-      location.reload();
+      //location.reload();
     }
   }
 })
 
+form.addEventListener('submit',(e) => e.preventDefault());
