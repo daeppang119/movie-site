ab608401b5cd044292cf18fff9fd23656c8c29b0
daeppang119 <dbsk9436@naver.com>
2023-10-20 11:51:21 +0900

    js 기능구현완료

--- api.js
+++ api.js
@@ -1,49 +1,55 @@
 // const API_KEY = '32e312ca42167aa3a612c46bc67be586'
 
 const options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMmUzMTJjYTQyMTY3YWEzYTYxMmM0NmJjNjdiZTU4NiIsInN1YiI6IjY1MmYyNTlkYTgwMjM2MDExYWM3YzBiYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.njfFp-h_Xr_eveVOIyI4_exwO1zWRtnpohvfcdIqVVA'
-  }
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMmUzMTJjYTQyMTY3YWEzYTYxMmM0NmJjNjdiZTU4NiIsInN1YiI6IjY1MmYyNTlkYTgwMjM2MDExYWM3YzBiYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.njfFp-h_Xr_eveVOIyI4_exwO1zWRtnpohvfcdIqVVA",
+  },
 };
 
-const searchBtnDOM = document.getElementById('searchBtn');
-const cardList = document.getElementById('cardList');
-const form = document.getElementById('form');
-const input = document.getElementById('searchInput');
-const searchBtn = document.getElementById('searchBtn');
+const searchBtnDOM = document.getElementById("searchBtn");
+const cardList = document.getElementById("cardList");
+const form = document.getElementById("form");
+const input = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
 
 function getTopRatedMovies(value) {
-  console.log('value is : ',value);
-  fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-    .then(response => response.json())
+  console.log("value => ", value);
+  fetch(
+    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+    options
+  )
+    .then((response) => response.json())
     .then((data) => {
-      if(value) {
-        const newData = {...data,results: data.results.filter(v => {
-          return v.title.toLowerCase().indexOf(value.toLowerCase()) !== -1;
-        })};
+      if (value) {
+        const newData = {
+          ...data,
+          results: data.results.filter((v) => {
+            return v.title.toLowerCase().indexOf(value.toLowerCase()) !== -1;
+          }),
+        }; // fetch해서 가져온 데이타와 input.value를 이용해서 원하는 데이터를 찾은뒤에 newData 변수에 할당한다.
         console.log(newData.results);
-        //data.results = data.results.filter(v => v.title === value);
-          renderMovies(newData);
-        return ;
+        renderMovies(newData);
+        return;
       }
       renderMovies(data);
-    }) // .then(data => renderMovies(data)) // 유난미 자꿈 움직이시ㅕㅁㄴ 저도 움직여요 ㅜㅜ
-    .catch(err => console.error(err));
+    }) // .then(data => renderMovies(data))
+    .catch((err) => console.error(err));
 }
 
 function renderMovies(data) {
-  // console.log(data)asdfasdfasdfasdfasdfasdfasd
+  // console.log(data)
   // console.log(data.results);
   // console.log(JSON.stringify(data));
-  cardList.innerHTML = '';
-  data.results.forEach(element => {
+  cardList.innerHTML = "";
+  data.results.forEach((element) => {
     // console.log(element.title);
 
     cardList.innerHTML += `
       <div class="movieCard" onclick="alert('영화 id : ${element.id}')">
-        <img src="https://image.tmdb.org/t/p/w300/${element.poster_path}" alt="">
+        <img src="https://image.tmdb.org/t/p/w500/${element.poster_path}" alt="${element.title}">
         <div class="movieInfo">
             <h3>${element.title}</h3>
             <span class="vote_average">평점 : ${element.vote_average}%</span>
@@ -53,30 +59,32 @@ function renderMovies(data) {
             ${element.overview}
         </div>
       </div>
-    `
+    `;
   });
 }
-// console.log('object' + "456" + '문자열더하기');
-// let a = '유나';
-// a += '박';
-// console.log(a);
 
 getTopRatedMovies();
 
-input.addEventListener('keyup', (e)=>{
-  e.preventDefault(); // 새로고침 방지
-  if(e.key === 'Enter') {
-    if(input.value !== '') {
-      // 유나님 이거 이렇게 하면 지금 똑같은 내용을 똑같이 랜더링을 하는 거잖아요
-      // 뭔가 value에 따른 조작을 해야하기 때문에
-      // 그부분을 생각하셔야 할 것 같아요
-
-      // 그런데 현재 코드에서 저라면 
-      getTopRatedMovies(input.value);
-    } else {
-      //location.reload();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  input.addEventListener("keyup", (e) => {
+    e.preventDefault(); // 새로고침 방지
+    if (e.key === "Enter") {
+      if (input.value !== "") {
+        getTopRatedMovies(input.value);
+        input.value = ""; // 검색 완료되면 검색창 빈값
+      } else {
+        window.location.reload();
+      }
     }
-  }
-})
+  });
 
-form.addEventListener('submit',(e) => e.preventDefault());
+  searchBtn.addEventListener("click", (e) => {
+    if (input.value !== "") {
+      getTopRatedMovies(input.value);
+      input.value = "";
+    } else {
+      window.location.reload();
+    }
+  });
+});
---
+++ spred.js
@@ -0,0 +1,28 @@
+/* const obj = {
+  name: "lee",
+  age: 20,
+};
+
+const obj2 = {
+    name: 'lee',
+    age: 20,
+}
+const obj3 = {
+    ...obj,
+
+}
+obj2.name = "kim";
+
+console.log(obj); */
+
+let object = {
+  name: "lee",
+  age: 20,
+  age: 15,
+  // 매무 많은 줄
+  age: 30,
+};
+console.log(object);
+
+"abcdefga".indexOf("z");
+// -1반환
